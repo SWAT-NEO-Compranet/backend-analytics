7a91eaa2e93adecea126636ad173b7222ab7c195
Juan Carlos García Esquivel <carlos.garcia.esquivel@live.com.mx>
2021-03-09 11:09:15 -0600

    feat(migrations): add default tables to queries

---
+++ app/Http/Controllers/DependenceDurationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DependenceDuration;
+use Illuminate\Http\Request;
+
+class DependenceDurationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DependenceDuration  $dependenceDuration
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DependenceDuration $dependenceDuration)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DependenceDuration  $dependenceDuration
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DependenceDuration $dependenceDuration)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DependenceDuration  $dependenceDuration
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DependenceDuration $dependenceDuration)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DependenceDuration  $dependenceDuration
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DependenceDuration $dependenceDuration)
+    {
+        //
+    }
+}
---
+++ database/factories/AmountsPerDependencyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AmountsPerDependency;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AmountsPerDependencyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AmountsPerDependency::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
